6b1e7f30e8f8b3962122421b8104178fc1bdfe1b
a3a256 <81564718+a3a256@users.noreply.github.com>
2022-12-15 23:17:20 +0000

    added vscode folder

--- searching/interpolation_search.cpp
+++ searching/interpolation_search.cpp
@@ -1,4 +1,24 @@
 #include <iostream>
+#include <vector>
+
+
+int search_vector(const std::vector<int> &vect, int lo, int hi, int target){
+    if (lo == hi){
+        if(vect[lo] == target){
+            return lo;
+        }else{
+            return -1;
+        }
+    }
+    int mid = lo + (target - vect[lo]) * (hi - lo)/(vect[hi] - vect[lo]);
+    if (vect[mid] == target){
+        return mid;
+    }else if(vect[mid] > target){
+        return search_vector(vect, lo, mid-1, target);
+    }else if(vect[mid] < target){
+        return search_vector(vect, mid+1, hi, target);
+    }
+}
 
 
 int search(int arr[], int lo, int hi, int target){
@@ -26,6 +46,17 @@ int main(){
     int arr[] = {1, 4, 6, 8, 9, 12, 16};
 
     std::cout << search(arr, (int)0, (int)6, (int)4) << '\n';
+
+    std::vector<int> arr1;
+    arr1.push_back(1);
+    arr1.push_back(3);
+    arr1.push_back(6);
+    arr1.push_back(9);
+    arr1.push_back(12);
+
+    // std::cout << arr1[0] << "\n";
+
+    std::cout << search_vector(arr1, (int)0, (int)arr1.size()-1, (int)9) << "\n";
 
     return 0;
 }
