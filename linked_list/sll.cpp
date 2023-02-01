7af52af698188ca4ba8983aec22bc1ee878a4010
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-02-01 23:40:57 +0000

    added constructor and insert function

--- linked_list/sll.cpp
+++ linked_list/sll.cpp
@@ -1,1 +1,31 @@
 #include <iostream>
+
+struct Node{
+    int data;
+    Node *next;
+
+    Node(int val=0) :data(val), next(nullptr){}
+
+    Node(int val, Node *tempNext):data(val), next(tempNext){}
+};
+
+
+class LinkedList{
+    Node *head;
+    public:
+
+        LinkedList():head(nullptr)
+        {
+        }
+
+        void insert(int val){
+            Node* new_node = new Node(val);
+
+            if (head == nullptr){
+                head = new_node;
+            }else{
+                head = head->next;
+                head = new_node;
+            }
+        }
+};
