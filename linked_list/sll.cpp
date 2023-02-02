7953d9da6adb2f2efca5a6e6b0ff7693a1d1bae4
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-02-02 00:40:48 +0000

    run tests, added insert function and display function

--- linked_list/sll.cpp
+++ linked_list/sll.cpp
@@ -20,12 +20,31 @@ class LinkedList{
 
         void insert(int val){
             Node* new_node = new Node(val);
-
-            if (head == nullptr){
+            Node* itr = head;
+            if (itr == nullptr){
                 head = new_node;
             }else{
-                head = head->next;
+                new_node->next = head;
                 head = new_node;
             }
         }
+
+        void display(){
+            Node* temp = head;
+            while(temp != nullptr){
+                std::cout << temp->data << "-->";
+                temp = temp->next;
+            }
+            std::cout << "null, so far\n";
+        }
 };
+
+
+int main(){
+    LinkedList lst;
+    lst.insert(1);
+    lst.insert(2);
+    lst.insert(3);
+
+    lst.display();
+}
