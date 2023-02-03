7e675ade17ec12935624ff38323c4d58e8154ae5
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-02-03 23:45:01 +0000

    added struct

--- tree_strcuctures/binary_search_tree.cpp
+++ tree_strcuctures/binary_search_tree.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
 struct Node{
-    
+    int root;
+    Node *left;
+    Node *right;
+    Node(int val=0) :root(val), left(nullptr), right(nullptr){}
+    Node(int val, Node *leftVal, Node *rightVal): root(val), left(leftVal), right(rightVal){}
 };
