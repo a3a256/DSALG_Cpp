7fd4c03334aa399946c982b1be79ce1f5ebc8e49
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-02-04 00:14:50 +0000

    reconstructed construct: '

--- tree_strcuctures/binary_search_tree.cpp
+++ tree_strcuctures/binary_search_tree.cpp
@@ -2,8 +2,39 @@
 
 struct Node{
     int root;
-    Node *left;
-    Node *right;
-    Node(int val=0) :root(val), left(nullptr), right(nullptr){}
-    Node(int val, Node *leftVal, Node *rightVal): root(val), left(leftVal), right(rightVal){}
+    struct Node *left;
+    struct Node *right;
+
+    Node(int root){
+        this->root = root;
+        this->left = nullptr;
+        this->right = nullptr;
+    }
+};
+
+
+class BinarySearchTree{
+
+    public:
+        Node *root;
+
+        BinarySearchTree(){
+            root = nullptr;
+        }
+
+        void insert(int val){
+            Node* new_leaf = new Node(val);
+            if(root == nullptr){
+                root = new_leaf;
+            }else{
+                Node* itr = root;
+                if(val >= itr->root){
+                    if(itr->left == nullptr){
+                        itr->left = new_leaf;
+                    }else{
+                        itr->left.insert(val);
+                    }
+                }
+            }
+        }
 };
