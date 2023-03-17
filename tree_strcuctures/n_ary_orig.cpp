b91a95874e24a6069ccd2d7e96ae466750f0700b
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-03-17 01:48:13 +0000

    added insertion and continuation of a level in a tree

--- tree_strcuctures/n_ary_orig.cpp
+++ tree_strcuctures/n_ary_orig.cpp
@@ -11,3 +11,32 @@ struct Node{
         this->children;
     }
 };
+
+
+class NTree{
+    Node* head;
+
+    NTree(){
+        head = nullptr;
+    }
+
+    void insert(int val){
+        Node* new_leaf = new Node(val);
+        if(head == nullptr){
+            head = new_leaf;
+            return;
+        }
+
+        std::string level, arr;
+        Node* focusNode = head;
+        if(focusNode->children.size() == 0){
+            focusNode->children.push_back(new_leaf);
+        }else{
+            std::cout << "Do you want to continue with the current level?";
+            std::cin >> level;
+            if(level == "y"){
+                focusNode->children.push_back(new_leaf);
+            }
+        }
+    }
+};
