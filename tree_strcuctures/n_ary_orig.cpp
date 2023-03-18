a1891b93adc9aa4d23eb75a441845b8d51bdbfe7
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-03-18 03:07:35 +0000

    Lack of visualisation and failed testing

--- tree_strcuctures/n_ary_orig.cpp
+++ tree_strcuctures/n_ary_orig.cpp
@@ -14,46 +14,57 @@ struct Node{
 
 
 class NTree{
-    Node* head;
+    public:
+        Node* head;
 
-    NTree(){
-        head = nullptr;
-    }
-
-    void insert(int val){
-        Node* new_leaf = new Node(val);
-        if(head == nullptr){
-            head = new_leaf;
-            return;
+        NTree(){
+            head = nullptr;
         }
 
-        std::string level, arr;
-        Node* focusNode = head;
-        Node* parent;
-        if(focusNode->children.size() == 0){
-            focusNode->children.push_back(new_leaf);
-        }else{
-            focusNode = head;
-            while(true){
-                parent = focusNode;
-                std::cout << "Do you want to continue with the current level?";
-                std::cin >> level;
-                if(level == "y"){
-                    focusNode->children.push_back(new_leaf);
-                    return;
-                }else{
-                    for(int i = 0; i<focusNode->children.size(); i++){
-                        std::cout << "Do you want to continue at " << focusNode->children[i]->val << "?";
-                        std::cin >> level;
-                        if(level == "y"){
-                            focusNode = focusNode->children[i];
-                            if(focusNode->children.size() == 0){
-                                focusNode->children.push_back(new_leaf);
+        void insert(int val){
+            Node* new_leaf = new Node(val);
+            if(head == nullptr){
+                head = new_leaf;
+                return;
+            }
+
+            std::string level, arr;
+            Node* focusNode = head;
+            Node* parent;
+            if(focusNode->children.size() == 0){
+                focusNode->children.push_back(new_leaf);
+            }else{
+                focusNode = head;
+                while(true){
+                    parent = focusNode;
+                    std::cout << "Do you want to continue with the current level?";
+                    std::cin >> level;
+                    if(level == "y"){
+                        focusNode->children.push_back(new_leaf);
+                        return;
+                    }else{
+                        for(int i = 0; i<focusNode->children.size(); i++){
+                            std::cout << "Do you want to continue at " << focusNode->children[i]->val << "?";
+                            std::cin >> level;
+                            if(level == "y"){
+                                focusNode = focusNode->children[i];
+                                if(focusNode->children.size() == 0){
+                                    focusNode->children.push_back(new_leaf);
+                                }
                             }
                         }
                     }
                 }
             }
         }
-    }
 };
+
+
+
+int main(){
+    NTree tree;
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(6);
+    tree.insert(7);
+}
