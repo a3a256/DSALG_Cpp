d0c93a6c81187271c236670baf8ea2a0a718c368
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-03-23 00:53:53 +0000

    Additional tests and added copy of insert method for future fixes

--- tree_strcuctures/n_ary_orig.cpp
+++ tree_strcuctures/n_ary_orig.cpp
@@ -21,6 +21,44 @@ class NTree{
             head = nullptr;
         }
 
+        void insert_copy(int val){
+            Node* new_leaf = new Node(val);
+            if(head == nullptr){
+                head = new_leaf;
+                return;
+            }
+
+            std::string level, arr;
+            Node* focusNode = head;
+            Node* parent;
+            if(focusNode->children.size() == 0){
+                focusNode->children.push_back(new_leaf);
+            }else{
+                focusNode = head;
+                while(true){
+                    parent = focusNode;
+                    std::cout << "Do you want to continue with the current level?";
+                    std::cin >> level;
+                    if(level == "y"){
+                        focusNode->children.push_back(new_leaf);
+                        return;
+                    }else{
+                        for(int i = 0; i<focusNode->children.size(); i++){
+                            std::cout << "Do you want to continue at " << focusNode->children[i]->val << "?";
+                            std::cin >> level;
+                            if(level == "y"){
+                                focusNode = focusNode->children[i];
+                                if(focusNode->children.size() == 0){
+                                    focusNode->children.push_back(new_leaf);
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         void insert(int val){
             Node* new_leaf = new Node(val);
             if(head == nullptr){
