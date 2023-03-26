52ec4be334b8504edf7abcf01130ad99af97ea55
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-03-26 23:35:15 +0100

    Implemented Binary Search using iterative approach

--- rust_part/binray_search.rs
+++ rust_part/binray_search.rs
@@ -1,5 +1,27 @@
 use std::convert::TryInto;
 
+fn iterative_search(vec: Vec<i32>, target: i32)->i32{
+    let mut start: i32 = 0;
+    let mut end: i32 = (vec.len()).try_into().unwrap();
+    end -= 1;
+    let mut _mid: i32 = 0;
+    let mut mid: usize = 0;
+
+    while start <= end{
+        _mid = (start + end)/2;
+        mid = (_mid).try_into().unwrap();
+        if vec[mid] == target{
+            return _mid;
+        }else if vec[mid] < target{
+            start = _mid + 1;
+        }else if vec[mid] > target{
+            end = _mid - 1;
+        }
+    }
+
+    return _mid;
+}
+
 fn binary_search(start: i32, end: i32, vec: Vec<i32>, target: i32)->i32{
     let _mid: i32 = (start+end)/2;
     let mid: usize = ((start+end)/2).try_into().unwrap();
@@ -29,5 +51,7 @@ fn main(){
 
     println!("{}", u/2);
 
-    println!("{}", binary_search(0 as i32, 6 as i32, vec, 13 as i32));
+    println!("{}", binary_search(0 as i32, 6 as i32, vec.clone(), 13 as i32));
+
+    println!("{}", iterative_search(vec.clone(), 13 as i32))
 }
