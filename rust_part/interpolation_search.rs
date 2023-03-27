e1007dacb5318feb261082e4dca017da35e5a1b4
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-03-28 00:20:55 +0100

    Added testing part for interpolation search

--- rust_part/interpolation_search.rs
+++ rust_part/interpolation_search.rs
@@ -11,6 +11,13 @@ fn interpolation_search(arr: Vec<i32>, lo: i32, hi: i32, target: i32) -> i32{
         }
     }
     let mut index_lo: usize = (lo).try_into().unwrap();
-    let mut index_hi: usize = (hi)/try_into()/unwrap();
+    let mut index_hi: usize = (hi).try_into().unwrap();
     let mut mid: i32 = lo + (target-arr[index_lo]) * (hi-lo)/(arr[index_hi] - arr[index_lo]);
+}
+
+
+fn main(){
+    let vec: Vec<i32> = vec![1, 4, 5, 7, 9, 13, 17];
+    let mut end: i32 = (vec.len()).try_into().unwrap();
+    println!("{}", interpolation_search(vec, 0 as i32, end, 13 as i32));
 }
