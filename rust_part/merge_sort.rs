4860f4b7fdec64b40a9427feb11237dbebfecb9d
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-03-30 00:53:11 +0100

    completed merge sort and tested

--- rust_part/merge_sort.rs
+++ rust_part/merge_sort.rs
@@ -3,20 +3,21 @@ use std::convert::TryInto;
 
 fn combine(left: Vec<i32>, right: Vec<i32>)->Vec<i32>{
     let mut i: i32 = 0;
-    let mut j: i32 = 0;
-    let mut i_index, j_index: usize;
+    let mut d: i32 = 0;
+    let mut i_index: usize;
+    let mut j_index: usize;
     let leftln: i32 = (left.len()).try_into().unwrap();
     let rightln: i32 = (right.len()).try_into().unwrap();
     let mut res: Vec<i32> = Vec::new();
-    while i<leftln && j<rightln{
-        if left[i] < right[j]{
-            i_index = (i).try_into().unwrap();
+    while i<leftln && d<rightln{
+        i_index = (i).try_into().unwrap();
+        j_index = (d).try_into().unwrap();
+        if left[i_index] < right[j_index]{
             res.push(left[i_index]);
             i += 1;
         }else{
-            j_index = (j).try_into().unwrap();
             res.push(right[j_index]);
-            j += 1;
+            d += 1;
         }
     }
 
@@ -26,10 +27,10 @@ fn combine(left: Vec<i32>, right: Vec<i32>)->Vec<i32>{
         i += 1;
     }
 
-    while j<rightln{
-        j_index = (j).try_into().unwrap();
+    while d<rightln{
+        j_index = (d).try_into().unwrap();
         res.push(right[j_index]);
-        j += 1;
+        d += 1;
     }
 
 
@@ -38,7 +39,7 @@ fn combine(left: Vec<i32>, right: Vec<i32>)->Vec<i32>{
 
 
 
-fn mergeSort(arr: Vec<i32>)->Vec<i32>{
+fn merge_sort(arr: Vec<i32>)->Vec<i32>{
     let length: i32 = (arr.len()).try_into().unwrap();
     if length == 1{
         return arr;
@@ -58,18 +59,32 @@ fn mergeSort(arr: Vec<i32>)->Vec<i32>{
     }
 
     while i<length{
-        j_index = (j).try_into().unwrap();
+        j_index = (i).try_into().unwrap();
         right.push(arr[j_index]);
         i += 1;
     }
 
 
-    return combine(mergeSort(left), mergeSort(right));
+    return combine(merge_sort(left), merge_sort(right));
 }
 
 
 fn main(){
     let vec: Vec<i32> = vec![2, 1, 4, 13, 11, 9];
 
-    println!("{}", mergeSort(vec));
+    let res: Vec<i32> = merge_sort(vec);
+
+    let mut i: i32 = 0;
+
+    let length: i32 = (res.len()).try_into().unwrap();
+
+    let mut index: usize;
+
+    while i<length{
+        index = (i).try_into().unwrap();
+        println!("{}", res[index]);
+        i += 1
+    }
+
+    // println!("{}", res[0 as usize]);
 }
