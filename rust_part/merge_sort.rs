03d5b67bc307a7e76e766ace3368c6b9af39de73
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-03-29 01:07:44 +0100

    Implemented merge sort intuitive approach

--- rust_part/merge_sort.rs
+++ rust_part/merge_sort.rs
@@ -1,6 +1,37 @@
 use std::convert::TryInto;
 
 
+fn combine(left: Vec<i32>, right: Vec<i32>)->Vec<i32>{
+    let mut i: i32 = 0;
+    let mut j: i32 = 0;
+    let leftln: i32 = (left.len()).try_into().unwrap();
+    let rightln: i32 = (right.len()).try_into().unwrap();
+    let mut res: Vec<i32> = Vec::new();
+    while i<leftln && j<rightln{
+        if left[i] < right[j]{
+            res.push(left[i]);
+            i += 1;
+        }else{
+            res.push(right[j]);
+            j += 1;
+        }
+    }
+
+    while i<leftln{
+        res.push(left[i]);
+        i += 1;
+    }
+
+    while j<rightln{
+        res.push(right[j]);
+        j += 1;
+    }
+
+
+    return res;
+}
+
+
 
 fn mergeSort(arr: Vec<i32>)->Vec<i32>{
     let length: i32 = (arr.len()).try_into().unwrap();
