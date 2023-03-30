e3c83168ad36c4f5a77f8031764d8902b98a9f12
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-03-31 00:56:20 +0100

    added rs file with bubble sort, I am running out of ideas

---
+++ rust_part/bubble_sort.rs
@@ -0,0 +1,14 @@
+use std::convert:TryInto;
+
+
+fn bubble_sort(arr: &mut Vec<i32>)->Vec<i32>{
+    let mut sorted: bool = false;
+    let mut i: i32 = 0;
+    let length: i32 = (arr.len()).try_into().unwrap();
+    let mut i_index: usize;
+    let mut j_index: usize;
+    while !(sorted){
+        sorted = true;
+        i = 0;
+    }
+}
