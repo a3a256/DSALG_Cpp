36434fcdb74e47c6036a83fbc1d0b8eb85249932
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-04-02 00:42:55 +0100

    Started implementing SLL

---
+++ rust_part/sll.rs
@@ -0,0 +1,17 @@
+use std::convert::TryInto;
+
+
+enum Option{
+    Some(T),
+    None,
+}
+
+
+struct Node{
+    let mut value: Option<i32> = None;
+    struct Node next;
+
+    fn Node(val: i32){
+        value = Some(value);
+    }
+}
