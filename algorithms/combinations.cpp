c5850ca6a93abda780b7e09ab84ffbba41955474
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-04-27 01:13:30 +0100

    implemented combinations soon to test

--- algorithms/combinations.cpp
+++ algorithms/combinations.cpp
@@ -7,4 +7,22 @@ std::vector<std::vector<int>> combinations(std::vector<int> vec, int n){
     if(n == 0){
         return l;
     }
+    std::vector<int> remlst;
+    std::vector<std::vector<int>> remain_comb;
+    remlst = vec;
+
+    int i, j, m;
+
+    for(i=0; i<vec.size(); i++){
+        m = vec[i];
+        remlst.erase(remlst.begin());
+        remain_comb = combinations(remlst, n-1);
+        temp.push_back(m);
+        for(j=0; j<remain_comb.size(); j++){
+            temp.insert(temp.end(), remain_comb[j].begin(), remain_comb[j].end());
+            l.push_back(temp);
+        }
+    }
+
+    return l;
 }
