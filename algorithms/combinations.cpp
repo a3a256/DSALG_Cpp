b82973c89c3ba3f935f1fe810ec20f06c1d099ac
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-04-27 01:04:47 +0100

    added comb algo

---
+++ algorithms/combinations.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> combinations(std::vector<int> vec, int n){
+    std::vector<std::vector<int>> l;
+    std::vector<int> temp;
+    if(n == 0){
+        return l;
+    }
+}
