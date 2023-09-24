47c7cd3354a091a6a0ec9f1e82bcd2020aa143c9
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-09-24 19:20:26 +0100

    added new

--- c_part/binary_search.c
+++ c_part/binary_search.c
@@ -17,12 +17,28 @@ int search_recursion(int* array, int size, int start, int end, int target){
     return -1;
 }
 
+int search_iterative(int* array, int size, int start, int end, int target){
+    int mid;
+    while(start<=end){
+        mid = (start+end)/2;
+        if(array[mid] == target){
+            return mid;
+        }else if(array[mid]<target){
+            start = mid+1;
+        }else{
+            end = mid-1;
+        }
+    }
+    return -1;
+}
+
 
 int main(){
     int arr[5] = {5, 7, 9, 12, 16};
     int size = 5;
     int pos = search_recursion(arr, size, 0, size-1, 7);
+    int pos1 = search_iterative(arr, size, 0, size-1, 9);
 
-    printf("The position is %d\n", pos);
+    printf("Result from recusrive approach is: %d\nIterative approach: %d\n", pos, pos1);
     return 0;
 }
