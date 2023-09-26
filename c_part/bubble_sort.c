da51c2bfc418fefa1a0f4d3c2ab5089e80e57d17
a3a256 <81564718+a3a256@users.noreply.github.com>
2023-09-26 18:18:15 +0100

    implemented bubble sort in c, but not tested yet

---
+++ c_part/bubble_sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int* sorted(int *arr, int size){
+    int sorted = 0, i, temp;
+    while(sorted == 0){
+        sorted = 1;
+        for(i=1; i<size; i++){
+            if(arr[i] < arr[i-1]){
+                sorted = 0;
+                temp = arr[i];
+                arr[i] = arr[i-1];
+                arr[i-1] = temp;
+            }
+        }
+    }
+
+    return arr;
+}
+
+
+int main(){
+    const int size = 5;
+    int arr[size] = {3, 2, 7, 1, 9};
+    int res[size] = sorted(arr, size);
+}
